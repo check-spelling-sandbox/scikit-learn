3e54f5ccf35f1782b57920d27d7c3be16f7f3c30
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-15 15:39:42 -0500

    spelling: longer
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- sklearn/metrics/_classification.py
+++ sklearn/metrics/_classification.py
@@ -129,7 +129,7 @@ def _check_targets(y_true, y_pred, sample_weight=None):
             )
         )
 
-    # We can't have more than one value on y_type => The set is no more needed
+    # We can't have more than one value on y_type => The set is no longer needed
     y_type = y_type.pop()
 
     # No metrics support "multiclass-multioutput" format
