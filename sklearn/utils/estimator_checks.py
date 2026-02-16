c7e6483d40510d6723c0305babf13bfad444cb3b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-15 20:03:42 -0500

    spelling: estimator
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -131,7 +131,7 @@ def _yield_api_checks(estimator):
 
     tags = get_tags(estimator)
     # This is commented out since it's the first check both
-    # `parametrize_with_checks` and `check_esitmator` do
+    # `parametrize_with_checks` and `check_estimator` do
     # anyway. But leaving it here as commented out to know
     # it's a part of the basic API.
     # yield check_estimator_cloneable
