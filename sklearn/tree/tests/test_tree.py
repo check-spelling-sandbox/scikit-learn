354cda499a4c856fcb8bb8452c60fcedf9504b51
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-15 20:35:02 -0500

    spelling: threshold
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -1268,7 +1268,7 @@ def test_almost_constant_feature(tree_cls):
     # Make sure that almost constant features are discarded.
     random_state = check_random_state(0)
     X = random_state.rand(10, 2)
-    # FEATURE_TRESHOLD=1e-7 is defined in sklearn/tree/_partitioner.pxd but not
+    # FEATURE_THRESHOLD=1e-7 is defined in sklearn/tree/_partitioner.pxd but not
     # accessible from Python
     feature_threshold = 1e-7
     X[:, 0] *= feature_threshold  # almost constant feature
