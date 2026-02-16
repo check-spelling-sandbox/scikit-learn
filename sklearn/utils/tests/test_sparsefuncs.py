16e12966f2a8c5c2758518f6e9185b92da836a5b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-15 22:20:11 -0500

    spelling: implicit
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- sklearn/utils/tests/test_sparsefuncs.py
+++ sklearn/utils/tests/test_sparsefuncs.py
@@ -991,7 +991,7 @@ def test_implicit_center_rmatmat(global_random_seed, centered_matrices):
     assert_allclose(X_dense_centered.T @ Y, X_sparse_centered.T @ Y)
 
 
-def test_implit_center_rmatvec(global_random_seed, centered_matrices):
+def test_implicit_center_rmatvec(global_random_seed, centered_matrices):
     X_sparse_centered, X_dense_centered = centered_matrices
     rng = np.random.default_rng(global_random_seed)
     y = rng.standard_normal(X_dense_centered.shape[0])
